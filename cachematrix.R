3833381c8e033b17ede5a5a9ff4e535aedd55877
Tarunima <tarunimap@gmail.com>
2018-03-27 12:20:57 -0700

    Submission for Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates Special Matrix Object that can Cache its Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    } 
+    get <- function()  x
+    
+    setinverse <- function(y) {
+        i <<- y
+    }
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Compute Inverse of Matrix returned by makeCacheMatrix if not already calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse
+    
+    if (!is.null(i)){
+        message("getting catched inverse")
+        i
+    }
+    A <- x$get
+    i <- ginv(A)
+    x$setinverse(i)
+    i
 }
